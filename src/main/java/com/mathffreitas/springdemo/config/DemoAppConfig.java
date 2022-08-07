3ff5cdb77ddd9bcdee8438cfcf5a53248c23cc16
Matheus Freitas <mathffreitas@hotmail.com>
2022-08-07 02:47:56 -0300

    Project structure

---
+++ src/main/java/com/mathffreitas/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.mathffreitas.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mathffreitas.springdemo")
+public class DemoAppConfig {
+
+}
